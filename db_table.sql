4a32badb95d34fce8935150643b689b1923234d2
Dix <Dix2491@yandex.ru>
2021-04-05 10:40:34 +0700

    Update db_table.sql

--- db_table.sql
+++ db_table.sql
@@ -1,25 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Хост: localhost
--- Время создания: Апр 05 2021 г., 06:38
--- Версия сервера: 5.7.27-30
--- Версия PHP: 7.1.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `u0478875_testing`
---
 
 -- --------------------------------------------------------
 
